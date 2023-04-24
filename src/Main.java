51fc09cf55482e2159e6d9b43c327f2035565309
JacquelineTorresR <l19021146@veracruz.tecnm.mx>
2023-04-24 13:28:42 -0600

    Colaboracion 1, Impresion de mensaje como colaborador
    JTR_dev

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo en Ingenieria de Software");
+        System.out.println("Esta es una aportacion como colaborador JTR_dev");
     }
 }
